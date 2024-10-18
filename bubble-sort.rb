f05aef50305cfc4e44d3dc072d7d9ccb677bd76f
Clayton Sweeten <cjsweeten101@gmail.com>
2024-10-18 13:05:05 -0600

    Add bubble sort method

---
+++ bubble-sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(arr)
+  k = 1
+  sorted = true
+  for i in 0..arr.length - 1  do
+    for i in 0..arr.length - k do
+      if arr[i+1] && arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        sorted = false
+      end
+    end
+    return arr if sorted
+    k+=1
+  end
+  arr
+end
+
+p bubble_sort([4,3,78,2,0,2, 6, 80, 43, 12, 60])
